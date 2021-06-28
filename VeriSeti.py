6649bca0609714bcc8643ec6173c8be342b4e81e
firattsn <86618412+firattsn@users.noreply.github.com>
2021-06-28 19:22:36 +0300

    Create VeriSeti.py

---
+++ VeriSeti.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video width
+cam.set(4, 480) # set video height
+face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# her kişi için bir id girilir.
+face_id = input('\n Kullanıcı ID girin ve  Enter'e basın. ')
+print("\n Yüz tanıma hazırlanıyor kameraya bakarak bekleyin.")
+# yüzdeki noktaların sayısı kaydediliyor.
+count = 0
+while(True):
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        count += 1
+        # veriseti klasörüne fotografı kaydeder.
+        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imshow('image', img)
+    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+    elif count >= 30: # Take 30 face sample and stop video
+         break
+print("\n [INFO] Programdan çıkılıyor.")
+cam.release()
+cv2.destroyAllWindows()
